c741ae9bde9d56268cfb1d4a9130b0560eb0827c
ArpDark <aritrapal073@gmail.com>
2024-02-10 01:36:57 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 const port=process.env.PORT||4173;
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    rollupOptions:{
+      input:"src/main.jsx"
+    }
+  },
   base: "/",
   server:{
     port:port
